96f395dad91afeafdb595d3298e53a7cb4a9e87d
Youjie Zheng <zyj578335934@gmail.com>
2026-04-05 21:35:23 +0800

    Update config for lab.einsia.ai

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://einsia.github.io',
-  base: '/Einsia-lab/',
+  site: 'https://lab.einsia.ai',
+  base: '/',
   output: 'static',
 });
