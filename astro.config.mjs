cf986cac0a776312bed132759d3b134cb51ed093
Qingle <lql24@mails.tsinghua.edu.cn>
2026-04-05 20:28:29 +0800

    fix: add trailing slash to base path to fix broken links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://einsia.github.io',
-  base: '/Einsia-lab',
+  base: '/Einsia-lab/',
   output: 'static',
 });
